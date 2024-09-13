23541b8bb3dde6e52157523fcffa9517006ef432
Jens Ochsenmeier <github@jens-ox.de>
2024-09-13 13:33:13 +0200

    initial plumbing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,18 @@
-import type { Config } from 'tailwindcss'
+import { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+import { radixThemePreset } from 'radix-themes-tw'
+import typography from '@tailwindcss/typography'
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+export default {
+  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  presets: [radixThemePreset],
+  plugins: [typography],
   theme: {
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono]
       }
     }
-  },
-  plugins: []
-}
-export default config
+  }
+} satisfies Config
---
+++ utils/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Source_Code_Pro } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
+export const mono = Source_Code_Pro({ variable: '--font-mono', subsets: ['latin'] })
